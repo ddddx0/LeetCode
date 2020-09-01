9ba803f3cabb4f4e68d202142352d21b4e20f369
ddddx0 <daniel_842791908@163.com>
2020-09-01 16:10:42 +0800

    20

---
+++ stack&queue/20. 有效的括号.go
@@ -0,0 +1,38 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println(isValid("{{{}}}}"))
+}
+
+func isValid(s string) bool {
+	var m = map[string]string{")": "(", "]": "[", "}": "{"}
+
+	stack := make([]string, 0)
+
+	for _, str := range s {
+		stackLen := len(stack)
+
+		// 如果为左括号，则直接放入栈中
+		if _, ok := m[string(str)]; !ok {
+			stack = append(stack, string(str))
+			continue
+		}
+
+		// 若为右括号，
+		// 如果栈中无元素，则绝对不匹配，返回false
+		// 若有元素，则比较栈中是否有对应右括号，有则弹出，无则返回false
+		if stackLen > 0 {
+			if stack[stackLen-1] == m[string(str)] {
+				stack = stack[:stackLen-1]
+			} else {
+				return false
+			}
+		} else {
+			return false
+		}
+
+	}
+	return len(stack) == 0
+}
---
+++ stack&queue/README.md
@@ -0,0 +1,11 @@
+844. 比较含退格的字符串
+https://leetcode-cn.com/problems/backspace-string-compare/
+
+232. 用栈实现队列
+https://leetcode-cn.com/problems/implement-queue-using-stacks/
+
+225. 用队列实现栈
+https://leetcode-cn.com/problems/implement-stack-using-queues/
+
+20. 有效的括号
+https://leetcode-cn.com/problems/valid-parentheses/
