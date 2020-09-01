9bd058da5e6711c3581d72811e61237441bb8fab
ddddx0 <daniel_842791908@163.com>
2020-09-01 16:24:57 +0800

    20

--- stack&queue/20. 有效的括号.go
+++ stack&queue/20. 有效的括号.go
@@ -6,6 +6,11 @@ func main() {
 	fmt.Println(isValid("{{{}}}}"))
 }
 
+// 1,如果为左括号，则直接放入栈中
+// 2,若为右括号，
+//	如果栈中无元素，则绝对不匹配，返回false
+//	若有元素，则比较栈中是否有对应右括号，有则弹出，无则返回false
+// 3,检查stack是否为空
 func isValid(s string) bool {
 	var m = map[string]string{")": "(", "]": "[", "}": "{"}
 
@@ -34,5 +39,7 @@ func isValid(s string) bool {
 		}
 
 	}
+
+	//检查stack是否为空
 	return len(stack) == 0
 }
